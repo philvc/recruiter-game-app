4db74c240bc04b1f48836d7b1361a0ddbfe2a17e
Phil <p.vancaloen@hotmail.com>
2020-05-12 09:50:26 +0200

    update mission progress & updateMissionV2 mutation

--- src/components/game/components/menus/components/menu-missions/components/mission/components/mission-10-job-offers/components/list-jobs/components/list-progress/index.tsx
+++ src/components/game/components/menus/components/menu-missions/components/mission/components/mission-10-job-offers/components/list-jobs/components/list-progress/index.tsx
@@ -1,12 +1,36 @@
 import * as React from 'react';
 
+// apollo
+import { useMutation, useApolloClient } from '@apollo/client';
+import { UPDATE_MISSION_V2 } from '../../../../../../../../../../../../../../graphql/mutations/server/updateMissionV2';
+import { GET_MISSION_CLIENT } from '../../../../../../../../../../../../../../graphql/queries/client/getMissionClient';
+
 const ListProgress = ({ jobs }: any) => {
 
-  const completedJob = jobs.filter((job: any) => job.url !== "" && job.name !== "").length
+  // client
+  const client = useApolloClient()
+  const { mission }: any = client.readQuery({ query: GET_MISSION_CLIENT })
+
+  // mutations
+  const [updateMissionV2] = useMutation(UPDATE_MISSION_V2)
+
+  const completedJobs = jobs.filter((job: any) => job.url !== "" && job.name !== "").length
+
+  // effect 
+  React.useEffect(() => {
+    updateMissionV2({
+      variables: {
+        id: mission.id,
+        field: 'progress',
+        data: completedJobs,
+      }
+    })
+  }, [jobs, updateMissionV2, mission, completedJobs])
 
   return (
     <div>
-      <p>{completedJob}/10</p>
+      <p>{completedJobs}/10</p>
+      {completedJobs === 10 && <button>Send for review</button>}
     </div>
   )
 }
---
+++ src/graphql/mutations/server/updateMissionV2.tsx
@@ -0,0 +1,31 @@
+import gql from 'graphql-tag'
+
+export const UPDATE_MISSION_V2 = gql`
+  mutation updateMissionV2($id: String, $data: StringOrIntOrBoolean, $field: String){
+    updateMissionV2(input: {id: $id, data:$data, field: $field}){
+      id
+      type
+      progress
+      gameId
+      isReviewed
+      isRecruiter
+      isLocked
+      status
+      score
+      isEvaluated
+      score
+      selectedJob {
+        id
+        url
+        missionId
+        isAccepted
+        rank
+        name
+        applicationProofUrl
+        isComplete
+        gameId
+      }
+      time
+    }
+  }
+`
