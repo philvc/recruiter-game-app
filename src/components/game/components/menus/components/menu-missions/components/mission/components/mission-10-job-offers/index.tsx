6b3ef9bcc7f2b33378a60bf94c103233929d291e
Phil <p.vancaloen@hotmail.com>
2020-05-17 13:02:13 +0200

    delete createNewMission

--- src/components/game/components/menus/components/menu-missions/components/mission/components/mission-10-job-offers/index.tsx
+++ src/components/game/components/menus/components/menu-missions/components/mission/components/mission-10-job-offers/index.tsx
@@ -9,20 +9,6 @@ import List from './components/list-jobs';
 
 const Mission10JobOffers = () => {
 
-  // const [updateGame] = useMutation(UPDATE_GAME, {
-  //   variables: { id: gameId },
-  //   onCompleted({ updateGame }) {
-  //     const { missionsAccomplished } = updateGame;
-
-  //     if (missionsAccomplished === 1 && game.missionsAccomplished === 0) {
-  //       createNewMission({ variables: { gameId, type: 'jobapplication' } })
-  //     }
-  //     localStorage.setItem('game', JSON.stringify(updateGame))
-  //   }
-  // })
-
-
-
   return (
     <div>
       <h1>10 Job Offers Mission</h1>
