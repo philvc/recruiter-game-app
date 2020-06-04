352e34ca2dc753c5755801f4eabfd98efb337d30
Phil <p.vancaloen@hotmail.com>
2020-06-04 11:45:14 +0200

    favicon uri

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const cache = new InMemoryCache({
 });
 
 // Apollo Links
-
+console.log('http link', process.env.HTTP_SERVER_LINK)
 const httpLink = new HttpLink({
   uri: process.env.HTTP_SERVER_LINK || "http://localhost:4000/"
 })
