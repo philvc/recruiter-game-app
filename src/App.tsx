ab61629e8edbafe4edb6d370d0e5412c58434ac4
Phil <p.vancaloen@hotmail.com>
2020-06-04 11:30:02 +0200

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        {/* <Router>
+        <Router>
           {player === undefined ?
             (<LoginV2 path='/' />)
             :
@@ -159,8 +159,7 @@ function App() {
           }
           <Game path='/games/*' />
           <NotFound default />
-        </Router> */}
-        Hello
+        </Router>
       </div>
     </ApolloProvider>
   );
