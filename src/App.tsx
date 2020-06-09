d81db600a73c1f157a7fb3ffd65824e180a97063
Phil <p.vancaloen@hotmail.com>
2020-06-09 14:16:36 +0200

    credentials omit

--- src/App.tsx
+++ src/App.tsx
@@ -32,10 +32,7 @@ const cache = new InMemoryCache({
 // Apollo Links
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_API_URL || "https://tenjobschallenge-server.herokuapp.com/" || "http://localhost:4000/",
-  credentials: 'include',
-  fetchOptions: {
-    mode: 'no-cors',
-  }
+  credentials: 'omit',
 })
 
 const wsLink = new WebSocketLink({
